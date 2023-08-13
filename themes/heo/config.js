5034a8bf3b7c6658ad20ff1d36ce9a8bd8ff3aab
GJJ <97382863+GaoJJ256@users.noreply.github.com>
2023-08-13 12:08:41 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -14,7 +14,7 @@ const CONFIG = {
   HERO_TITLE_2: '与思维认知',
   HERO_TITLE_3: 'gaojj.cn',
   HERO_TITLE_4: '新版上线',
-  HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
+  HERO_TITLE_5: '来和GJJ做朋友！',
   HERO_TITLE_LINK: 'https://gaojj.cn',
 
   // 英雄区显示三个置顶分类
@@ -40,14 +40,14 @@ const CONFIG = {
 
   // 用户技能图标
   GROUP_ICONS: [
-    {
-      title_1: 'AfterEffect',
-      img_1: '/images/heo/20239df3f66615b532ce571eac6d14ff21cf072602.webp',
-      color_1: '#989bf8',
-      title_2: 'Sketch',
-      img_2: '/images/heo/2023e0ded7b724a39f12d59c3dc8fbdc7cbe074202.webp',
-      color_2: '#ffffff'
-    },
+    // {
+    //   title_1: 'AfterEffect',
+    //   img_1: '/images/heo/20239df3f66615b532ce571eac6d14ff21cf072602.webp',
+    //   color_1: '#989bf8',
+    //   title_2: 'Sketch',
+    //   img_2: '/images/heo/2023e0ded7b724a39f12d59c3dc8fbdc7cbe074202.webp',
+    //   color_2: '#ffffff'
+    // },
     {
       title_1: 'Docker',
       img_1: '/images/heo/20231108a540b2862d26f8850172e4ea58ed075102.webp',
@@ -56,22 +56,22 @@ const CONFIG = {
       img_2: '/images/heo/2023e4058a91608ea41751c4f102b131f267075902.webp',
       color_2: '#4082c3'
     },
-    {
-      title_1: 'FinalCutPro',
-      img_1: '/images/heo/20233e777652412247dd57fd9b48cf997c01070702.webp',
-      color_1: '#ffffff',
-      title_2: 'Python',
-      img_2: '/images/heo/20235c0731cd4c0c95fc136a8db961fdf963071502.webp',
-      color_2: '#ffffff'
-    },
-    {
-      title_1: 'Swift',
-      img_1: '/images/heo/202328bbee0b314297917b327df4a704db5c072402.webp',
-      color_1: '#eb6840',
-      title_2: 'Principle',
-      img_2: '/images/heo/2023f76570d2770c8e84801f7e107cd911b5073202.webp',
-      color_2: '#8f55ba'
-    },
+    // {
+    //   title_1: 'FinalCutPro',
+    //   img_1: '/images/heo/20233e777652412247dd57fd9b48cf997c01070702.webp',
+    //   color_1: '#ffffff',
+    //   title_2: 'Python',
+    //   img_2: '/images/heo/20235c0731cd4c0c95fc136a8db961fdf963071502.webp',
+    //   color_2: '#ffffff'
+    // },
+    // {
+    //   title_1: 'Swift',
+    //   img_1: '/images/heo/202328bbee0b314297917b327df4a704db5c072402.webp',
+    //   color_1: '#eb6840',
+    //   title_2: 'Principle',
+    //   img_2: '/images/heo/2023f76570d2770c8e84801f7e107cd911b5073202.webp',
+    //   color_2: '#8f55ba'
+    // },
     {
       title_1: 'illustrator',
       img_1: '/images/heo/20237359d71b45ab77829cee5972e36f8c30073902.webp',

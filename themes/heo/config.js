9125596942d43d4950350633f0050af60d2ea2de
GJJ <97382863+GaoJJ256@users.noreply.github.com>
2024-08-06 19:29:11 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -23,9 +23,9 @@ const CONFIG = {
   HEO_HERO_TITLE_LINK: 'blog.gaojj.cn',
 
   // // 英雄区显示三个置顶分类
-  // HEO_HERO_CATEGORY_1: { title: '读书笔记', url: '/tag/读书笔记' },
-  // HEO_HERO_CATEGORY_2: { title: '碎片杂文', url: '/tag/碎片杂文' },
-  // HEO_HERO_CATEGORY_3: { title: '学习思考', url: '/tag/学习思考' },
+  HEO_HERO_CATEGORY_1: { title: '读书笔记', url: '/tag/读书笔记' },
+  HEO_HERO_CATEGORY_2: { title: '碎片杂文', url: '/tag/碎片杂文' },
+  HEO_HERO_CATEGORY_3: { title: '学习思考', url: '/tag/学习思考' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',

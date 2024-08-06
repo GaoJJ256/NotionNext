84fcf1c6fab4272b3c4ed2ab8600e817bf223c99
GJJ <97382863+GaoJJ256@users.noreply.github.com>
2024-08-06 19:14:25 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -18,8 +18,8 @@ const CONFIG = {
   HEO_HERO_TITLE_1: '分享编程',
   HEO_HERO_TITLE_2: '与生活',
   HEO_HERO_TITLE_3: 'blog.gaojj.cn',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: '',
+  HEO_HERO_TITLE_4: '你好👋',
+  HEO_HERO_TITLE_5: '我是GJJ',
   HEO_HERO_TITLE_LINK: 'blog.gaojj.cn',
 
   // // 英雄区显示三个置顶分类
